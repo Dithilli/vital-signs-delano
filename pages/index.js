db530ceb0a38e210de41050501afcd15ef1be984
David Szarzynski <David.Szarzynski@gmail.com>
2023-09-26 13:02:33 -0500

    first push with background logo thing

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+import logo from '../assets/images/homepage.png'
 
 export default function Home() {
+  console.log(logo); 
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Vital Signs Delano" />
+        <Image
+          src={logo}
+          alt="Picture of the author"
+        />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
 
