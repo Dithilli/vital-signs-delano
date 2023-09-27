62e7238f3ed7ea2a81851f27e4bb241a943ca5af
David Szarzynski <David.Szarzynski@gmail.com>
2023-09-26 21:54:24 -0500

    Works great

--- components/Logo.js
+++ components/Logo.js
@@ -2,26 +2,35 @@ import Image from 'next/image'
 import vitalSigns from '../assets/images/Vitalsigns.svg' 
 import tagline from '../assets/images/DesignTagline.svg'
 import phoneNumber from '../assets/images/Phone Number.svg'
-import styles from './Footer.module.css'
+import combinedLogo from '../assets/images/combinedLogo.svg'
 
 export default function Logo() {
+  const styles = {
+    vitalSigns :{
+      position: 'absolute',
+  },
+  tagline :{
+      position: 'absolute',
+  },
+  phoneNumber: {
+      position: 'absolute'
+  },
+  container: {
+      position: 'relative',
+      width: '100%',
+      margin: 'auto',
+  }
+}
+
   return (
     <>
+    <div className={styles.container}>
       <Image
-        className={styles.vitalSigns}
-        src={vitalSigns}
+        style={styles.vitalSigns}
+        src={combinedLogo}
         alt="Vital Signs Logo"
       />
-      <Image
-        className={styles.tagline}
-        src={tagline}
-        alt="Vital Signs Logo"
-      />
-        <Image
-        className={styles.phoneNumber}
-        src={phoneNumber}
-        alt="Vital Signs Logo"
-      />
+      </div>
     </>
   )
 }
