eecbc255aa7a8a5850e23e39d35b4797d797bfda
David Szarzynski <David.Szarzynski@gmail.com>
2024-01-31 13:33:50 -0600

    updated names

--- components/Picturelinks.js
+++ components/Picturelinks.js
@@ -1,27 +1,154 @@
 import React from 'react';
 import { Box, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
+import  InteriorGraphics from '/assets/WM_Interior_graphics.png';
+import signSystems from '/assets/sign_systems.png';
+import delToro from '/assets/del_toro.jpg';
+import nationWide from '/assets/nation_wide_install.jpg';
+import realEstate from '/assets/real_estate.png';
+import monuments from '/assets/monuments.jpg';
 
 const PictureLinks = () => {
   return (
     <Box sx={{ flexGrow: 1, p: 2 }}>
       <Grid container spacing={2}>
-        {[...Array(6)].map((_, i) => (
-          <Grid item xs={12} sm={6} md={4} key={i}>
+      <Grid item xs={12} sm={6} md={4} key="1">
             <Card>
               <CardMedia
                 component="img"
-                height="140"
-                image={`https://via.placeholder.com/150`}
-                alt={`Placeholder ${i}`}
+                image={InteriorGraphics.src}
+                alt={`Placeholder`}
               />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Picture {i + 1}
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom component="div">
+                  Interior & Exterior Graphics
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-        ))}
+
+          <Grid item xs={12} sm={6} md={4} key="2">
+            <Card>
+              <CardMedia
+                component="img"
+                image={signSystems.src}
+                alt={`Placeholder`}
+              />
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom variant="h6" component="div">
+                  Sign Systems
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4} key="3">
+            <Card>
+              <CardMedia
+                component="img"
+                image={delToro.src}
+                alt={`Placeholder`}
+              />
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom variant="h6" component="div">
+                  Exhibits & Displays
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4} key="4">
+            <Card>
+              <CardMedia
+                component="img"
+                image={nationWide.src}
+                alt={`Placeholder`}
+              />
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom variant="h6" component="div">
+                  Nation-Wide Installation
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          
+          <Grid item xs={12} sm={6} md={4} key="5">
+            <Card>
+              <CardMedia
+                component="img"
+                image={realEstate.src}
+                alt={`real estate signage`}
+              />
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom variant="h6" component="div">
+                  Real Estate Signage
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={4} key="6">
+            <Card>
+              <CardMedia
+                component="img"
+                image={monuments.src}
+                alt={`Monuments`}
+              />
+              <CardContent sx={{
+        textTransform: 'uppercase',
+        backgroundColor: 'black',
+        color: 'white',
+        textAlign: 'center',
+        padding: '16px',
+        textOverflow: 'ellipsis',
+        width: '100%', // Ensure the Typography component spans the full width
+      }}>
+                <Typography gutterBottom variant="h6" component="div">
+                  Monuments to Man's Hubris
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+
       </Grid>
     </Box>
   );
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import {blah} from '../assets/VITAL_LOGOwAddress.svg'
+import Blah from '../assets/vitalLogo.svg'
 
 const Sidebar = () => {
   return (
-    <Box sx={{ width: { md: '25%' }, p: 2, bgcolor: 'grey.200' }}>
-      <Box
-  component="img"
-  sx={{
-    height: 233,
-    width: 350,
-    maxHeight: { xs: 233, md: 167 },
-    maxWidth: { xs: 350, md: 250 },
-  }}
-  alt="Logo"
-  src={blah}
-/>
+    <Box sx={{ width: { md: '75%' }, p: 2, bgcolor: 'grey.200' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', p: 2 }}>
+        <Blah/>
+      </Box>
     </Box>
   );
 };
