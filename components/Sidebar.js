6cfd4695e9d1baa2d7f89978596884e51f82b2c4
David Szarzynski <David.Szarzynski@gmail.com>
2024-02-05 13:46:25 -0600

    New logo that's not responsive for iOS17

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import Blah from '../assets/vitalLogo.svg'
+import Blah from '../assets/vital_logo_nonrespond.svg'
 
 const Sidebar = () => {
   return (
