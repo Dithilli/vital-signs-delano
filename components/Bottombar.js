16d4731d350402606d11b9d11a6e4794819e0d6a
David Szarzynski <David.Szarzynski@gmail.com>
2024-02-05 16:39:28 -0600

    added links on the bottom

--- components/Bottombar.js
+++ components/Bottombar.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography, Link } from '@mui/material';
 
 const BottomBar = () => {
   return (
-    <Box sx={{ p: 2, bgcolor: '#84C8E6', mt: 2 }}>
-      <p>Details in Bottom Bar</p>
+    <Box sx={{ p: 2, bgcolor: '#84C8E6', mt: 2, textAlign: 'center' }}>
+      <Typography variant="body1">
+        call: <Link href="tel:+17639725101" style={{ color: 'inherit' }}>763 972-5101</Link> or email: <Link href="mailto:enquires@vitalvisual.com" style={{ color: 'inherit' }}>enquires@vitalvisual.com</Link>
+      </Typography>
     </Box>
   );
 };
