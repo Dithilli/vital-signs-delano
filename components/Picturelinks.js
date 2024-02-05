0dd8675b179af544ff21e3f193787f7c7a11b9eb
David Szarzynski <David.Szarzynski@gmail.com>
2024-02-05 14:05:54 -0600

    Working now much better

--- components/Picturelinks.js
+++ components/Picturelinks.js
@@ -7,147 +7,68 @@ import nationWide from '/assets/home_page/nation_wide_install.jpg';
 import realEstate from '/assets/home_page/realestate_signs.png';
 // import monuments from '/assets/home_page/monuments.jpg';
 
+const cardData = [
+  {
+    image: InteriorGraphics,
+    alt: `Placeholder`,
+    title: `Interior & Exterior Graphics`,
+  },
+ {
+    image: signSystems,
+    alt: `Placeholder`,
+    title: `Sign Systems`,
+  },
+   {
+    image: delToro,
+    alt: `Placeholder`,
+    title: `Exhibits & Displays`,
+  },
+   {
+    image: nationWide,
+    alt: `Placeholder`,
+    title: `Nation-Wide Installation`,
+  },
+   {
+    image: realEstate,
+    alt: `Placeholder`,
+    title: `Real Estate Signage`,
+  },
+//   6: {
+//     image: monuments,
+//     alt: `Placeholder`,
+//     title: `Monuments`
+// }
+];
+
 const PictureLinks = () => {
   return (
-    <Box sx={{ flexGrow: 1, p: 2 }}>
+    <Box sx={{ flexGrow: 2, p: 2 }}>
       <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} md={4} key="1">
-            <Card>
+        {cardData.map((card, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+             <Card key={index} style={{ maxWidth: 600, maxHeight: 'auto' }}>
               <CardMedia
                 component="img"
-                image={InteriorGraphics.src}
-                alt={`Placeholder`}
+                height="300"
+                image={card.image.src}
+                alt={card.alt}
               />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom component="div">
-                  Interior & Exterior Graphics
+                <CardContent sx={{
+                  textTransform: 'uppercase',
+                  backgroundColor: 'black',
+                  color: 'white',
+                  textAlign: 'center',
+                  padding: '16px',
+                  textOverflow: 'ellipsis',
+                  width: '100%', // Ensure the Typography component spans the full width
+                }}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {card.title}
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-
-          <Grid item xs={12} sm={6} md={4} key="2">
-            <Card>
-              <CardMedia
-                component="img"
-                image={signSystems.src}
-                alt={`Placeholder`}
-              />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Sign Systems
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} key="3">
-            <Card>
-              <CardMedia
-                component="img"
-                image={delToro.src}
-                alt={`Placeholder`}
-              />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Exhibits & Displays
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12} sm={6} md={4} key="4">
-            <Card>
-              <CardMedia
-                component="img"
-                image={nationWide.src}
-                alt={`Placeholder`}
-              />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Nation-Wide Installation
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          
-          <Grid item xs={12} sm={6} md={4} key="5">
-            <Card>
-              <CardMedia
-                component="img"
-                image={realEstate.src}
-                alt={`real estate signage`}
-              />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Real Estate Signage
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-{/* 
-          <Grid item xs={12} sm={6} md={4} key="6">
-            <Card>
-              <CardMedia
-                component="img"
-                image={monuments.src}
-                alt={`Monuments`}
-              />
-              <CardContent sx={{
-        textTransform: 'uppercase',
-        backgroundColor: 'black',
-        color: 'white',
-        textAlign: 'center',
-        padding: '16px',
-        textOverflow: 'ellipsis',
-        width: '100%', // Ensure the Typography component spans the full width
-      }}>
-                <Typography gutterBottom variant="h6" component="div">
-                  Monuments to Man's Hubris
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid> */}
+        ))}
 
       </Grid>
     </Box>
