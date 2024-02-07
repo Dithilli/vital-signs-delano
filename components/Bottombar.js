4eec6fcb75b842688311b7b5ec1f88cc45060650
David Szarzynski <David.Szarzynski@gmail.com>
2024-02-07 11:00:40 -0600

    fixed bottombar

--- components/Bottombar.js
+++ components/Bottombar.js
@@ -4,8 +4,8 @@ import { Box, Typography, Link } from '@mui/material';
 const BottomBar = () => {
   return (
     <Box sx={{ p: 2, bgcolor: '#84C8E6', mt: 2, textAlign: 'center' }}>
-      <Typography variant="body1">
-        call: <Link href="tel:+17639725101" style={{ color: 'inherit' }}>763 972-5101</Link> or email: <Link href="mailto:enquires@vitalvisual.com" style={{ color: 'inherit' }}>enquires@vitalvisual.com</Link>
+      <Typography variant="h6" >
+        <strong>call: </strong> <Link href="tel:+17639725101" style={{ color: 'inherit' }}>763 972-5101</Link> <strong> or email: </strong> <Link href="mailto:enquires@vitalvisual.com" style={{ color: 'inherit' }}>enquires@vitalvisual.com</Link>
       </Typography>
     </Box>
   );
